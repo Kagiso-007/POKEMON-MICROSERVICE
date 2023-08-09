087515b9ba5374a883def846083b2539dcc00a2a
Kagiso-007 <kraseroka100@gmail.com>
2023-08-09 19:05:28 +0200

    update CrossOrigin

--- src/main/java/com/kagiso/pokemon/controller/PokemonController.java
+++ src/main/java/com/kagiso/pokemon/controller/PokemonController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4100"})
+@CrossOrigin(origins = "*")
 public class PokemonController {
 
     @Autowired
